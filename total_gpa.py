b0d17a4d3f9ad470740abcf96bbdfc21faaa6bf4
Ejims Chigbogu <stanleychigbogu@gmail.com>
2025-05-09 21:03:18 +0100

    fixed a little issues i noticed in the GPA calculations

--- total_gpa.py
+++ total_gpa.py
@@ -1,16 +1,15 @@
 import csv
 
 
-with open('gpa_main.csv', 'r') as f:
-        rows = csv.DictReader(f)
-        
-        cu = 0
-        gpa = 0
-        for row in rows:
-                cu += int(row['credit_unit'])
-                gpa += int(row['credit_unit']) * int(row['grade_point'])
-        
-        total_gpa = gpa / cu
-        
-        print(total_gpa)
+with open("gpa_main.csv", "r") as f:
+    rows = csv.DictReader(f)
 
+    cu = 0
+    gpa = 0
+    for row in rows:
+        cu += int(row["credit_unit"])
+        gpa += int(row["credit_unit"]) * int(row["grade_point"])
+
+    total_gpa = gpa / cu
+
+    print(total_gpa)
